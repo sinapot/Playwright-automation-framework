c869d8bf2ff72107f79b02527cf95218550f96cb
sinapot <omai_edison@yahoo.com>
2022-08-20 11:34:22 +1200

    added testdata as user data source

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,19 +2,20 @@
 
 // @ts-check
 const { test, expect } = require('@playwright/test');
-const { HomePage } = require('./pages/homePage');
+const { HomePage } = require('../pages/homePage');
+import { user } from './testdata';
+
 
 test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
   
   const homepage = new HomePage(page);
   
   await homepage.goto();
-
   await expect(page).toHaveTitle(/ParaBank/);
   
   // Click the get started link.
-  await homepage.userName.type('john');
-  await homepage.password.type('demo');
+  await homepage.userName.type(user.username);
+  await homepage.password.type(user.password);
   await homepage.loginBtn.click();
   
   // Expects the URL to contain intro.
---
+++ tests/testdata.js
@@ -0,0 +1,5 @@
+export const user = {
+    username: 'john',
+    password: 'demo',
+
+}
